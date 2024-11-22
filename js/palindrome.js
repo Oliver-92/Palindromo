60359d55d755bdd92a1a28b7a4735abe5f7bfcf1
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2024-11-22 18:50:38 -0300

    final version

--- js/palindrome.js
+++ js/palindrome.js
@@ -2,22 +2,23 @@ const userInput = document.getElementById('text-input');
 const checkButton = document.getElementById('check-btn');
 const result = document.getElementById('result');
 
-
-
-const checkInput = input => {
-    const originalInput = input;
+const checkInput = () => {
+    const input = userInput.value; // Capturar el valor del campo de texto
 
     if (input === '') {
         alert('Please input a value');
         return;
     }
-    result.replaceChildren();
+
+    userInput.value = "";
 
     const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
 
-    
+    if (lowerCaseStr === lowerCaseStr.split("").reverse().join("")) {
+        result.innerText = input + " is a palindrome";
+    } else {
+        result.innerText = input + " is not a palindrome";
+    }
+};
 
-
-}
-
-
+checkButton.addEventListener('click', checkInput)
